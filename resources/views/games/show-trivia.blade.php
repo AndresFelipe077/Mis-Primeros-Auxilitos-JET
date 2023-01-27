411fbb0b9d4cb3fe0c3ae2c42bece77b7f3b63bd
Andres Felipe <pizoluligoa@gmail.com>
2023-01-26 19:13:03 -0500

    Se pone interfaz de algunos cards en trivias y transiciones

--- resources/views/games/show-trivia.blade.php
+++ resources/views/games/show-trivia.blade.php
@@ -1,13 +1,16 @@
+<link id="image-head" rel="shortcut icon" href="{{ asset('img/menu/challengue2.png') }}" type="image/x-icon">
 <x-app-layout>
 
     @section('title','Games')
 
     <x-slot name="header">
+        <x-header />
     </x-slot>
 
     @livewire('game.show-trivia')
 
     <x-slot name="footer">
+        <x-footer />
     </x-slot>
 
 </x-app-layout>
--- resources/views/livewire/game/show-trivia.blade.php
+++ resources/views/livewire/game/show-trivia.blade.php
@@ -1,4 +1,88 @@
 @section('title', 'Crear Trivia')
 <div>
-   
-</div>
+
+    <link rel="stylesheet" href="{{ asset('css/hoverImage.css') }}">
+
+    <div class="container">
+        <div class="row g-3 ">
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card m-4 text-center">
+                    <div class=" card-body shadow ">
+                        <h5 class="card-title">Tipos de curitas</h5>
+                        <div class="contenedor rounded">
+                            <img class="imagen rounded img-fluid" src="{{ asset('img/menu/challengue2.png') }}" alt="Image of trivia"
+                                width="250px" height="250px">
+                        </div>
+                        <p class="card-text">Trivia con preguntas de curas.</p>
+                        <div class="text-center">
+                            <a href="#" class="btn btn-primary ">Vamos!!! <i class="bi bi-lightbulb-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card m-4 text-center">
+                    <div class=" card-body shadow ">
+                        <h5 class="card-title">Tipos de curitas</h5>
+                        <div class="contenedor rounded">
+                            <img class="imagen rounded img-fluid" src="{{ asset('img/menu/challengue2.png') }}" alt="Image of trivia"
+                                width="250px" height="250px">
+                        </div>
+                        <p class="card-text">Trivia con preguntas de curas.</p>
+                        <div class="text-center">
+                            <a href="#" class="btn btn-primary ">Vamos!!! <i class="bi bi-lightbulb-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card m-4 text-center">
+                    <div class=" card-body shadow ">
+                        <h5 class="card-title">Tipos de curitas</h5>
+                        <div class="contenedor rounded">
+                            <img class="imagen rounded img-fluid" src="{{ asset('img/menu/challengue2.png') }}" alt="Image of trivia"
+                                width="250px" height="250px">
+                        </div>
+                        <p class="card-text">Trivia con preguntas de curas.</p>
+                        <div class="text-center">
+                            <a href="#" class="btn btn-primary ">Vamos!!! <i class="bi bi-lightbulb-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="card m-4 text-center">
+                    <div class=" card-body shadow ">
+                        <h5 class="card-title">Tipos de curitas</h5>
+                        <div class="contenedor rounded">
+                            <img class="imagen rounded img-fluid" src="{{ asset('img/menu/challengue2.png') }}" alt="Image of trivia"
+                                width="250px" height="250px">
+                        </div>
+                        <p class="card-text">Trivia con preguntas de curas.</p>
+                        <div class="text-center">
+                            <a href="#" class="btn btn-primary ">Vamos!!! <i class="bi bi-lightbulb-fill"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4">
+                <div id="effect-hover">
+                    <div class="img">
+                        <img src="{{ asset('img/menu/challengue2.png') }}" class="img-fluid">
+                        <div id="efecto">
+                            <h2>Efecto</h2>
+                            <p>lorem ipsum atsais dase.</p>
+                        </div>
+                    </div>
+                    </sectdivion>
+                </div>
+            </div>
+        </div>
+
+    </div>
