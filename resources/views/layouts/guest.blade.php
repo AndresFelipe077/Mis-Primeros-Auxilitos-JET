3d50fb6343c59657816842d52940c3a555ee00a3
Andres Felipe <pizoluligoa@gmail.com>
2023-03-11 11:01:41 -0500

    seguridad en todos los meta

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
 
