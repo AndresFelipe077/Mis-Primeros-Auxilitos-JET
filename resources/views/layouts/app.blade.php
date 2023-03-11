61aa2c841dac4532fef4b110f78062e565a9047c
Andres Felipe <pizoluligoa@gmail.com>
2023-03-11 10:48:40 -0500

    css cabecera

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>@yield('title') - {{ config('app.name') }}</title>
 
     {{-- <link rel="shortcut icon" href="{{ asset('img/botiquin.png') }}" type="image/x-icon"> --}}
