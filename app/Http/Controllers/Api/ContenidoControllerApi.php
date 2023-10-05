15b6e6ad8b275b721e007a662e716c4c56cbaf53
AndresFelipe077 <pizoluligoa@gmail.com>
2023-10-05 14:34:18 -0500

    Add code by orderBy desc content for api rest

--- app/Http/Controllers/Api/ContenidoControllerApi.php
+++ app/Http/Controllers/Api/ContenidoControllerApi.php
@@ -10,100 +10,97 @@ use Illuminate\Http\Request;
 
 class ContenidoControllerApi extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $contenidos = Contenido::all();
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $contenidos = Contenido::orderBy('id', 'desc')->get();
 
-        return response()->json($contenidos);
+    return response()->json($contenidos);
+  }
+
+
+  /**
+   * This function get content about user by id
+   * @param (int) $userId
+   * @author Andres Felipe Pizo Luligo
+   */
+  public function contenidosByUser($userId): JsonResponse
+  {
+    $user = User::findOrFail($userId);
+    $contenidos = $user->contenidos;
+
+    return response()->json($contenidos);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $contenido = Contenido::create($request->all());
+
+    return response()->json($contenido, 200);
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $content = Contenido::find($id);
+
+    if (!$content) {
+      return response()->json(['error' => 'Content not found']);
     }
 
-    /**
-     * This function get content about user by id
-     * @param (int) $userId
-     * @author Andres Felipe Pizo Luligo
-     */
-    public function contenidosByUser($userId): JsonResponse
-    {
-        $user = User::findOrFail($userId);
-        $contenidos = $user->contenidos;
+    return response()->json($content, 200);
+  }
 
-        return response()->json($contenidos);
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+
+    $contenido = Contenido::findOrFail($id);
+
+    if (!$contenido) {
+      return response()->json(['error' => 'Content not found'], 404);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $contenido = Contenido::create($request->all());
+    $contenido->update($request->all());
 
-        return response()->json($contenido, 200);
+    return response()->json($contenido, 200);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $data = Contenido::findOrFail($id);
+
+    if ($data) {
+      $data->delete();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        $content = Contenido::find($id);
-
-        if(!$content) {
-            return response()->json(['error' => 'Content not found']);
-        }
-
-        return response()->json($content, 200);
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-
-        $contenido = Contenido::findOrFail($id);
-
-        if(!$contenido) {
-            return response()->json(['error' => 'Content not found'], 404);
-        }
-
-        $contenido->update($request->all());
-
-        return response()->json($contenido, 200);
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $data = Contenido::findOrFail($id);
-
-        if($data){
-            $data->delete();
-        }
-
-        return response()->json("Delete content successfully!!!");
-
-    }
-    
+    return response()->json("Delete content successfully!!!");
+  }
 }
