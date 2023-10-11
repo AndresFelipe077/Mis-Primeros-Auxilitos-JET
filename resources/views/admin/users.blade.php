3bbef2e923fc99267532c90ac7afa2d49b333664
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-10-11 11:52:43 -0500

    Add modal in user_admin

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -29,6 +29,9 @@
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 @section('content_header')
     <h1>Mis Primeros Auxilitos</h1>
 @stop
@@ -97,20 +100,12 @@
 
                                                 <td class="align-middle">
                                                     {{-- Toca crear un get para ir a otra vista y ahi si editar --}}
-                                                    <form action="" method="POST" enctype="multipart/form-data">
-                                                        @csrf
-                                                        @method('put')
-                                                        <button class="btn btn-success text-success bg-success rounded m-2">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                                height="16" fill="currentColor"
-                                                                class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                                                <path
-                                                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                                                <path fill-rule="evenodd"
-                                                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                                            </svg>
-                                                        </button>
-                                                    </form>
+                                                    <div class="form-check form-switch m-3">
+                                                        <input class="form-check-input" type="checkbox" role="switch"
+                                                            id="flexSwitchCheckDefault">
+                                                        <label class="form-check-label" for="flexSwitchCheckDefault">Admin</label>
+                                                    </div>
+
                                                 </td>
 
                                                 <td class="align-middle">
@@ -173,6 +168,10 @@
             });
         </script>
         <script src="{{ asset('js_css_admin/toast-delete-user.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </script>
+
 
     </body>
 @endsection
--- resources/views/admin/users_admin.blade.php
+++ resources/views/admin/users_admin.blade.php
@@ -32,6 +32,9 @@
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
 @section('content_header')
     <h1>Mis Primeros Auxilitos</h1>
 @stop
@@ -94,21 +97,80 @@
                                                 </td>
 
                                                 <td class="align-middle">
-                                                    {{-- Toca crear un get para ir a otra vista y ahi si editar --}}
-                                                    <form action="" method="POST" enctype="multipart/form-data">
-                                                        @csrf
-                                                        @method('put')
-                                                        <button class="btn btn-success text-success bg-success rounded m-2">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16"
-                                                                height="16" fill="currentColor"
-                                                                class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                                                <path
-                                                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                                                <path fill-rule="evenodd"
-                                                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                                            </svg>
-                                                        </button>
-                                                    </form>
+                                                    
+                                                    <button class="btn btn-success text-success bg-success rounded mb-3"
+                                                    data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                                        height="16" fill="currentColor" class="bi bi-pencil-square"
+                                                        viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                        <path fill-rule="evenodd"
+                                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                    </svg>
+                                                </button>
+
+                                                <div class="modal fade" id="exampleModal" tabindex="-1"
+                                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                                                    Editar usuario - admin 🤗</h1>
+                                                                <button type="button" class="btn-close"
+                                                                    data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+
+                                                                <div
+                                                                    class="container text-center mx-auto animate__animated animate__swing rounded">
+
+
+                                                                    <div class="row">
+
+                                                                        <div class="col-sm-2 text-center"></div>
+
+                                                                        <div class="col-sm-8">
+                                                                            <div class="card">
+                                                                                {{-- <form
+                                                                                    action="{{ route('admin.update.image', $contenido) }}"
+                                                                                    method="POST"
+                                                                                    enctype="multipart/form-data">
+                                                                                    @csrf
+                                                                                    @method('put')
+
+                                                   
+                                                                                    <div class="modal-footer">
+                                                                                        <button type="button"
+                                                                                            class="btn btn-danger"
+                                                                                            data-bs-dismiss="modal">Cancelar</button>
+                                                                                        <button type="submit"
+                                                                                            class="btn btn-success"
+                                                                                            style="pointer">Actualizar
+                                                                                        </button>
+                                                                                    </div>
+
+
+                                                                                </form> --}}
+
+                                                                            </div>
+                                                                        </div>
+
+                                                                        <div class="col-sm-2 text-center"></div>
+
+                                                                    </div>
+
+                                                                </div>
+
+                                                            </div>
+
+                                                        </div>
+
+                                                    </div>
+                                                </div>
+
+                                                </div>
+
                                                 </td>
 
                                                 <td class="align-middle">
@@ -170,6 +232,10 @@
             });
         </script>
         <script src="{{ asset('js_css_admin/toast-delete-user.js') }}"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        </script>
 
     </body>
 @endsection
