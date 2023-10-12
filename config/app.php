5633a889cc533351bf37121eff61e4c459ef734f
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-10-12 08:48:38 -0500

    Install composer with Collective

--- config/app.php
+++ config/app.php
@@ -215,8 +215,8 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'Html'      => Collective\Html\HtmlFacade::class,
         'Image'     => Intervention\Image\Facades\Image::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
     ])->toArray()
