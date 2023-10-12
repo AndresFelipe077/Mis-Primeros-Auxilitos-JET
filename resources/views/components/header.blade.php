c03c699fd5cf76ca708f4ab9d11e30a4e0f0b644
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-10-12 08:11:45 -0500

    Add comment in header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -6,7 +6,7 @@
                     alt="" width="60px" height="50px"></a>
             <div id="iconsvg" class="d-flex position-absolute ">
 
-                @if (Auth::user()->hasRole('Admin'))
+                @if (Auth::user()->hasRole('Admin')) {{-- Validation for admins --}}
                     <a class="rounded-circle bg-transparent" href="{{ route('admin') }}">
                         <img src="{{ asset('img/admin/admin.png') }}" alt="Admin" width="50px" height="50px">
                     </a>
