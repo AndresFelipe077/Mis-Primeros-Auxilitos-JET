48ba6cd2ac0dbf097982f4036edebb7d3b0512e5
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-10-13 08:51:42 -0500

    commented button of report in view statistics

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -31,8 +31,8 @@
                         <!-- Page Heading -->
                         <div class="d-sm-flex align-items-center justify-content-between mb-4">
                             <h1 class="h3 mb-0 text-gray-800">Auxilitos estadisticas</h1>
-                            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                    class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                            {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                    class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
                         </div>
 
                         <!-- Content Row -->
