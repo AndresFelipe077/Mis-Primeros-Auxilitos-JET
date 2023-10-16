82a45e081ae89d6163db3b14794464ca4a2059b6
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-10-16 11:34:41 -0500

    Create new function for update user in profile

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
+
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, mixed>
+   */
+  public function rules()
+  {
+
+    $user = new User();
+
+    return [
+      'name'            => 'required',
+      'email'           => "required",//|email|unique:users,email,$user->id",
+      'genero'          => 'nullable',
+      'fechaNacimiento' => 'nullable',
+      'description'     => 'nullable',
+    ];
+  }
+
+}
