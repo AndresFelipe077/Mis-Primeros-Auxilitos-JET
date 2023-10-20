3abb34a1020e3f145f2607f5e7fe6c0f0f2b0226
Jhon smith <jhonsenapro@gmail.com>
2023-10-20 10:57:04 -0500

    se agregan elementos ala seccion de jeugos

--- public/js/JuegoAdivina.js
+++ public/js/JuegoAdivina.js
@@ -1,69 +1,76 @@
-const app = document.getElementById('app')
+document.addEventListener('DOMContentLoaded', function() {
+    // Llama a la función ramdon() cuando se carga la página
+    ramdon();
+});
+
+const app = document.getElementById('app');
 let templateHTML = '';
-let ArrayCard = []
-let ArrayFruit = []
-const btn = document.getElementById('btn')
-let listFruit = ['💉', '👸', '🚑', '⛑️', '🚑', '🏥', '💉', '👨‍⚕️', '👨‍⚕️', '⛑️', '👸', '🏥']
-let count=0;
-console.log(ArrayCard.length)
-listFruit.forEach(fruit=>(
-    templateHTML+=`
+let ArrayCard = [];
+let ArrayFruit = [];
+const btn = document.getElementById('btn');
+let listFruit = ['💉', '👸', '🚑', '⛑️', '🚑', '🏥', '💉', '👨‍⚕️', '👨‍⚕️', '⛑️', '👸', '🏥'];
+let count = 0;
+let isChecking = false; // Nueva variable para rastrear la comprobación
+console.log(ArrayCard.length);
+listFruit.forEach((fruit) => (
+    templateHTML += `
     <div class="card">
         <div class="sides front"></div>
         <div class="sides back">${fruit}</div>
     </div>
     `
-))
+));
 
-app.innerHTML=templateHTML
+app.innerHTML = templateHTML;
 
-app.addEventListener('click',(e)=>{
-    let value = e.target.matches('.front')
+app.addEventListener('click', (e) => {
+    let value = e.target.matches('.front');
 
-    if(value && ArrayCard.length<=2){
-      console.log(ArrayCard)
-       if(count<=1){
-          let ElementCard = e.target.parentElement
-        let fruit = ElementCard.children[1].textContent
-        
-        ElementCard.classList.add('rotate')
-        ArrayCard=[...ArrayCard,ElementCard]
-        ArrayFruit=[...ArrayFruit,fruit]
+    if (value && ArrayCard.length < 2 && !isChecking) { // Evitar abrir más de dos cartas y comprobar durante la verificación
+        if (count < 2) {
+            let ElementCard = e.target.parentElement;
+            let fruit = ElementCard.children[1].textContent;
 
-        VerificationsCards();
-       }
+            ElementCard.classList.add('rotate');
+            ArrayCard = [...ArrayCard, ElementCard];
+            ArrayFruit = [...ArrayFruit, fruit];
+
+            VerificationsCards();
+        }
     }
-})
+});
 
-const VerificationsCards = ()=>{
-    if(ArrayCard.length>1){
-        if(ArrayFruit[0] === ArrayFruit[1]){
-            ArrayCard=""
-            ArrayFruit=""
-        }else{
+const VerificationsCards = () => {
+    if (ArrayCard.length === 2) { // Verificar después de abrir dos cartas
+        isChecking = true; // Iniciar la comprobación
+        if (ArrayFruit[0] === ArrayFruit[1]) {
+            ArrayCard = [];
+            ArrayFruit = [];
+            isChecking = false; // Finalizar la comprobación
+        } else {
             setTimeout(() => {
-                ArrayCard[0].classList.remove('rotate')
-                ArrayCard[1].classList.remove('rotate')
-                ArrayCard=""
-                ArrayFruit=""
+                ArrayCard[0].classList.remove('rotate');
+                ArrayCard[1].classList.remove('rotate');
+                ArrayCard = [];
+                ArrayFruit = [];
+                isChecking = false; // Finalizar la comprobación
             }, 800);
         }
     }
-}
-
+};
 
 const ramdon = () => {
     for (let index = app.children.length; index >= 0; index--) {
-      app.appendChild(app.children[(Math.random() * index) | 0]);
+        app.appendChild(app.children[(Math.random() * index) | 0]);
     }
-  };
+};
 
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll('.card');
 
-  btn.addEventListener('click',()=>{
+btn.addEventListener('click', () => {
     ramdon();
 
-    for(let index of cards){
-        index.classList.remove('rotate')
+    for (let index of cards) {
+        index.classList.remove('rotate');
     }
-  })
+});
