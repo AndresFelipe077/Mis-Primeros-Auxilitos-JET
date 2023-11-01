638cc3ca9c8320e39a4cd67dfd298dc96c2845b3
Jhon smith <jhonsenapro@gmail.com>
2023-11-01 16:21:41 -0500

    cambios

--- resources/views/contenido/premium/contenidoPremium.blade.php
+++ resources/views/contenido/premium/contenidoPremium.blade.php
@@ -95,8 +95,39 @@
         </div>
     </div>
 
+
+    {{-- <div id="paypal-button-container-P-66S47642J3354442YMVAZHCQ"></div>
+    <script src="https://www.paypal.com/sdk/js?client-id=AUgDyfyqBycdUb_XUsANjBmAuHfJxUHAgx2ZStgncmUrraGbz8Y_8cRZZV8MDSQz_qnKGe9307b-fPOe&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
+    <script>
+      paypal.Buttons({
+          style: {
+              shape: 'pill',
+              color: 'gold',
+              layout: 'vertical',
+              label: 'subscribe'
+          },
+          createSubscription: function(data, actions) {
+            return actions.subscription.create({
+              /* Creates the subscription */
+              plan_id: 'P-66S47642J3354442YMVAZHCQ'
+            });
+          },
+          onApprove: function(data, actions) {
+            alert(data.subscriptionID); // You can add optional success message for the subscriber here
+          }
+      }).render('#paypal-button-container-P-66S47642J3354442YMVAZHCQ'); // Renders the PayPal button
+    </script> --}}
+
+
+
+
+
+
     <x-slot name="footer">
         <x-footer />
     </x-slot>
+
+
+
 
 </x-app-layout>
