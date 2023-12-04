6d437a01463f37a02497d934784eba88e879d00c
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-12-04 18:57:14 -0500

    download apk

--- app/Http/Controllers/DownloadApp.php
+++ app/Http/Controllers/DownloadApp.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class DownloadApp extends Controller
 {
-    //
+  public function downloadApk()
+  {
+    $rutaArchivo = public_path('apk/mis-primeros-auxilitos.apk');
+    return response()->download($rutaArchivo, 'mis-primeros-auxilitos.apk');
+  }
 }
